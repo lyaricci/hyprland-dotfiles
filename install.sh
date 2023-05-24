acf38252693fb8cb9983a871f7d47c0b922512aa
Nathálya Ricci <nathalyaricci@gmail.com>
2023-05-24 00:51:52 -0300

    Reset configs

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -sf ~/hyprland-dotfiles/HyprV/ ~/.config/
+ln -sf ~/hyprland-dotfiles/hypr/ ~/.config/
+ln -sf ~/hyprland-dotfiles/kitty/ ~/.config/
+ln -sf ~/hyprland-dotfiles/mako/ ~/.config/
+ln -sf ~/hyprland-dotfiles/swaylock/ ~/.config/
+ln -sf ~/hyprland-dotfiles/waybar/ ~/.config/
+ln -sf ~/hyprland-dotfiles/wlogout/ ~/.config/
+ln -sf ~/hyprland-dotfiles/wofi/ ~/.config/
