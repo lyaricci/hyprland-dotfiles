ed514f06ab7dfa2b01b47a3c5f02044c046d6093
Nathálya Ricci <nathalyaricci@gmail.com>
2023-05-30 18:43:14 -0300

    Change neotree position

---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+        width = 30,
+        auto_expand_width = false,
+      },
+    },
+  },
+}
