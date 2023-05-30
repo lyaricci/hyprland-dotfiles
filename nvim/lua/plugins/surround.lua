69b88bcaf647a208534a81f8465053c80b7d089e
Nathálya Ricci <nathalyaricci@gmail.com>
2023-05-29 21:23:58 -0300

    Change mini.surround mappings

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,10 +1,26 @@
 return {
-  "kylechui/nvim-surround",
-  version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  event = "VeryLazy",
-  config = function()
-    require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
-    })
-  end,
+	-- "kylechui/nvim-surround",
+	-- version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	-- event = "VeryLazy",
+	-- config = function()
+	--   require("nvim-surround").setup({
+	--     -- Configuration here, or leave empty to use defaults
+	--   })
+	-- end,
+	--
+	-- Change mini.surround mappings
+	{
+		"echasnovski/mini.surround",
+		opts = {
+			mappings = {
+				add = "gsa",
+				delete = "gsd",
+				find = "gsf",
+				find_left = "gsF",
+				highlight = "gsh",
+				replace = "gsr",
+				update_n_lines = "gsn",
+			},
+		},
+	},
 }
